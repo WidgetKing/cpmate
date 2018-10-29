1124181b97f2c197d02035f0ce18be615df3dfe8
WidgetKing <$tSdw$LqS0&#>
2018-10-29 09:52:56 +0800

    First Commit

---
+++ builds/development/Infosemantics_CpMate.js
@@ -0,0 +1,16 @@
+/**
+ * Captivate Mate
+ * Version: 0.0.1
+ * Build: 1
+ * Written By: Tristan Ward
+ * Company: Infosemantics
+ * Licence: See licence document provided with CpMate purchase
+ * Copyright: Tristan Ward 2018
+ */
+(function () {
+    function isInCaptivate() {
+
+        return window.parent.hasOwnProperty("cp");
+
+    }
+}());
---
+++ components/scripts/js/dev/dev.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tristan
+ * Date: 10/25/18
+ * Time: 5:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
