42937464e037043452dddca8f6499912c5c03df9
WidgetKing <$tSdw$LqS0&#>
2018-10-29 10:11:28 +0800

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tristan
+ * Date: 10/29/18
+ * Time: 10:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+var gulp = require("gulp"),
+    gutil = require("gulp-util");
+
+gulp.task("log", function () {
+    gutil.log("Hello world!");
+});
